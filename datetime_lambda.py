84cd01f2329a6ce281f3f46c28240c4c44c72bad
SaikiranThorrikonda <saikiran.thorrikonda@gmail.com>
2022-10-15 22:13:49 -0400

    Update datetime_lambda.py

--- datetime_lambda.py
+++ datetime_lambda.py
@@ -1,1 +1,29 @@
+import time
+import json
+import boto3
+
+def lambda_handler(event,context):
+    dynamodb = boto3.resource('dynamodb')
+    
+#function definition
+def lambda_handler(event,context):
+    dynamodb = boto3.resource('dynamodb')
+    
+    #table created with table name
+    table = dynamodb.Table('pollinate') 
+    
+	#time and date functions
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+	
+    print("Execution Successful...!", time_string)
+    
+    #inserting values into table with key-value pair
+    response = table.put_item(
+       Item={
+            'date_time': str(time_string),
+            
+        }
+    )
+    return response
 
